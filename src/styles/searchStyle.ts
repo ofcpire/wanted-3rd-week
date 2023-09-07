28149a56ff6cf4caa10887923529ef6354efdb1f
ofcpire <ofcpire@gmail.com>
2023-09-07 15:01:58 +0900

    Design : 추천 검색어 컴포넌트 디자인

--- src/styles/searchStyle.ts
+++ src/styles/searchStyle.ts
@@ -4,7 +4,8 @@ const SearchForm = styled.form`
   background-color: white;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 0px 25px;
   border-radius: 25px;
   width: 300px;
   height: 50px;
@@ -26,12 +27,29 @@ const SearchStyle = styled.div`
   position: relative;
 `;
 
-const SearchRecoStyle = styled.div`
+const SearchRecoListStyle = styled.div`
+  width: 300px;
   border-radius: 10px;
   padding: 10px;
   background-color: white;
   position: absolute;
   transform: translateY(50px);
+  .list-header {
+    color: grey;
+    font-size: small;
+    padding: 10px;
+  }
 `;
 
-export { SearchForm, SearchPageStyle, SearchStyle, SearchRecoStyle };
+const SearchRecoItemStyle = styled.div<SearchRecoItemProps>`
+  background-color: ${props => (props.$select === props.$idx ? 'lightblue' : 'white')};
+  color: ${props => (props.$select === props.$idx ? 'white' : 'black')};
+  padding: 5px;
+`;
+
+interface SearchRecoItemProps {
+  $select: number;
+  $idx: number;
+}
+
+export { SearchForm, SearchPageStyle, SearchStyle, SearchRecoListStyle, SearchRecoItemStyle };
