2f418dd7cca14fdbfc7477e1a52b75e486b67aaf
ofcpire <ofcpire@gmail.com>
2023-09-07 15:03:59 +0900

    Feat : 리스트 각 아이템 분리 및 스타일링

---
+++ src/components/SearchRecoItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SearchRecoItemStyle } from '../styles/searchStyle';
+
+interface RecoItemProps {
+  sickNm: string;
+  select: number;
+  idx: number;
+}
+
+export default function SearchRecoItem({ sickNm, select, idx }: RecoItemProps) {
+  return (
+    <SearchRecoItemStyle $select={select} $idx={idx}>
+      {sickNm}
+    </SearchRecoItemStyle>
+  );
+}
